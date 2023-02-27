dde8bf1037755f9556332a85476d046a044582a3
Jose Alcivar <jose.e.alcivar.c@outlook.com>
2023-02-27 12:39:29 -0500

    commit02

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,8 @@
+// alert("Hola Mundo");
+
+// let nombre = prompt("¿Cual es tu nombre?", "Sin Nombre");
+// console.log( nombre );
+
+const seleccion = confirm("¿Está seguro de hacer esto?");
+
+console.log(seleccion);
